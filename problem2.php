ef371d6bedf53b4ede4b43c4ce6cbd9d75038547
dhruv1110 <dpate171@nyit.edu>
2016-03-18 01:32:37 -0400

    Code modified

--- problem2.php
+++ problem2.php
@@ -1,5 +1,17 @@
 <?php
 $employees = array('Travis' => 29, 'John' => 30, 'Manny' => 24, 'Gabriel' => 20, 'Yogi' => 22);
+//Sorting array alphabatically
 ksort($employees);
-print_r(json_encode($employees));
+$result = array();
+//Created a loop for iterating keys and values
+foreach ($employees as $key => $value) {
+	$temp_array = array();
+    $temp_array['name'] = $key;
+    $temp_array['age'] = $value;
+    $result[] = $temp_array;
+	$temp_array = NULL;
+}
+echo("<pre>");
+print_r(json_encode($result));
+echo("</pre>");
 ?>
